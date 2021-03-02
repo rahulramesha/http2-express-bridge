7b12ff992ccfb5a8292235c47890ffea76990e0b
rahulramesha <rahul.r10061995@gmail.com>
2021-03-02 18:35:22 +0530

    fixing test certs env variables

--- test/support/env.js
+++ test/support/env.js
@@ -1,6 +1,6 @@
 
 process.env.NODE_ENV = 'test';
 process.env.NO_DEPRECATION = 'body-parser,express';
-process.env.CERT_KEY = 'C:/test/privateKey.key';
-process.env.CERT = 'C:/test/certificate.crt';
+process.env.CERT_KEY = './test/support/certs/privateKey.key';
+process.env.CERT = './test/support/certs/certificate.crt';
 
